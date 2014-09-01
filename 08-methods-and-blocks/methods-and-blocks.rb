574f2b0ea8eb717c8207f18ea2d97c9c69f3c8a8
David G Chaves <davidgchaves@gmail.com>
2014-09-01 13:12:05 +0200

    Methods and Blocks: A Block converted to a Proc and used later

--- 08-methods-and-blocks/methods-and-blocks.rb
+++ 08-methods-and-blocks/methods-and-blocks.rb
@@ -4,3 +4,18 @@ end
 
 puts double(3) { |val| "I got #{val}" }           #--> I got 6
 puts double("tom") { |val| "Then I got #{val}" }  #--> Then I got tomtom
+
+class TaxCalculator
+  def initialize(name, &block)
+    @name, @block = name, block
+  end
+
+  def get_tax(amount)
+    "#@name on #{amount} = #{ @block.call amount }"
+  end
+end
+
+tc = TaxCalculator.new("Sales tax") { |amt| amt * 0.075 }
+
+puts tc.get_tax 100   #--> Sales tax on 100 = 7.5
+puts tc.get_tax 250   #--> Sales tax on 250 = 18.75
